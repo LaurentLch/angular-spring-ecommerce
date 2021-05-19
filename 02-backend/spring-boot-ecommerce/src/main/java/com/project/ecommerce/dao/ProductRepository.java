a9bc57f03812fbb2be41bc292e7a5097f7a8790e
LaurentLch <laurentlonchay@gmail.com>
2021-05-19 16:14:16 +0200

    Section 15 | Search for products by keyword

--- 02-backend/spring-boot-ecommerce/src/main/java/com/project/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/project/ecommerce/dao/ProductRepository.java
@@ -1,8 +1,20 @@
 package com.project.ecommerce.dao;
 
 import com.project.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    //QUERY METHOD
+    //Find by category id ex: http://localhost:8080/api/products/search/findByCategoryId?id=1
+    Page<Product> findByCategoryId(@RequestParam("id")Long id, Pageable pageable);
+
+    //Find by name ex: http://localhost:8080/api/products/search/findByNameContaining?name=Python
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 
 }
---
+++ 03-frontend/angular-ecommerce/src/app/services/product/product.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import {Product} from "../../common/product";
+import {ProductCategory} from "../../common/product-category";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+
+  constructor(private httpClient: HttpClient) { }
+
+  private getProducts(searchUrl: string) {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+
+  getProductList(theCategoryId: number): Observable<Product[]> {
+    // need to build URL based on category id
+    //http://localhost:8080/api/products/search/findByCategoryId?id=1
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
+
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    // need to build URL based on the keyword
+    // http://localhost:8080/api/products/search/findByNameContaining?name=Data
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+}
+
+//use the interface to unwrap the JSON from Spring Data REST _embedded entry
+interface GetResponseProducts {
+  _embedded: {
+    products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
+  }
+}
+
